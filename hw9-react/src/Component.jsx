38380df7c0261c302af8330c5c6cc360ac0ca063
Natalia Chinarova <philonena7@gmail.com>
2021-04-08 16:07:37 +0300

    create Clicker

---
+++ hw9-react/src/Component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Component.css'
+
+class Component extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            counter: document.querySelector('.counter') || 0
+        }
+    }
+
+    plusClick = () => {
+        this.setState((prevState) => ({counter: prevState.counter +1}))
+    }
+
+    refreshClick = () => {
+        this.setState({counter: 0})
+    }
+
+    minusClick = () => {
+        this.setState((prevState) => ({counter: prevState.counter -1}))
+    }
+
+    render() {
+        return (
+            <div className="component">
+                <p className="counter">{this.state.counter}</p>
+                <div className="buttons">
+                    <input type="button" onClick={this.plusClick} className="plusButton" value="+"/>
+                    <input type="button" onClick={this.refreshClick} className="refreshButton" value="&#8634;"/>
+                    <input type="button" onClick={this.minusClick} className="minusButton" value="-"/>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default Component;
---
+++ hw9-react/src/Components.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Component from './Component';
+import './Components.css';
+
+function Components() {
+     return (
+        <div className="components">
+            <Component />
+            <Component />
+            <Component />
+            <Component />
+            <Component />
+        </div>
+     )
+}
+
+export default Components;
